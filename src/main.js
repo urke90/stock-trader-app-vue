bff1a88232e43117d92eb1247022450f037b5f1f
urke90 <urosbijelic90@gmail.com>
2021-09-07 17:46:59 +0200

    added vuex store, action for getting stocks

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import { routes } from './routes.js';
+import store from './store/store.js';
 
 Vue.use(VueRouter);
 
@@ -16,5 +17,6 @@ import App from './App.vue'
 new Vue({
     el: '#app',
     router,
+    store,
     render: h => h(App)
 });
