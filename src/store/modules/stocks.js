6fe9b8cf527bb1d9a104ff07200992a418f70366
urke90 <urosbijelic90@gmail.com>
2021-09-18 17:12:45 +0200

    buy stock functionality as well as rendering portfolio stocks

--- src/store/modules/stocks.js
+++ src/store/modules/stocks.js
@@ -18,7 +18,8 @@ const actions = {
         commit('SET_STOCKS', stocks);
     },
     buyStock({ commit }, order) {
-
+        console.log('kurac', order);
+        commit('BUY_STOCK', order);
     },
     randomizeStocks({ commit }) {
         commit('RND_STOCKS')
--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,20 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import stocks from './modules/stocks.js';
+import portfolio from './modules/portfolio.js';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
-        stocks
+        stocks,
+        portfolio
     }
 });
+
+// buy stock povezati prvo
+// napraviti novu comp za portfolio kao stock
+// 1. treba da ima i quantity
+// 2. sell umesto buy i treba da se poveze sa vuex
+// probati sa mapActions da uradim
+// U PORTFOLIO KOMPONENTI TREBA DA SE GETTERS STOCK PORTFOLIO
